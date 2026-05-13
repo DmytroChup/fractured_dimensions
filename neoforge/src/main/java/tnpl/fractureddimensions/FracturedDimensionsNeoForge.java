fb43ffe4640fcbbe36084a9b26d79a8b479a6ced
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2026-05-13 02:05:22 +0200

    Clean up boilerplate code in loader entrypoints

--- neoforge/src/main/java/tnpl/fractureddimensions/FracturedDimensionsNeoForge.java
+++ neoforge/src/main/java/tnpl/fractureddimensions/FracturedDimensionsNeoForge.java
@@ -1,6 +1,5 @@
 package tnpl.fractureddimensions;
 
-
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
@@ -8,14 +7,6 @@ import net.neoforged.fml.common.Mod;
 public class FracturedDimensionsNeoForge {
 
     public FracturedDimensionsNeoForge(IEventBus eventBus) {
-
-        // This method is invoked by the NeoForge mod loader when it is ready
-        // to load your mod. You can access NeoForge and Common code in this
-        // project.
-
-        // Use NeoForge to bootstrap the Common mod.
-        Constants.LOG.info("Hello NeoForge world!");
         CommonClass.init();
-
     }
 }
