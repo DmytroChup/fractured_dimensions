65245c6b05880c2a14c8259eed5a0510024be26d
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2026-05-23 13:30:47 +0200

    Add Raw Azurite and Azurite Ingot items

--- common/src/main/java/tnpl/fractureddimensions/registry/ModItems.java
+++ common/src/main/java/tnpl/fractureddimensions/registry/ModItems.java
@@ -17,6 +17,8 @@ public class ModItems {
     public static final RegistryObject<Item, Item> METEORITE_SHARD = ITEMS.register("meteorite_shard", () -> new Item(itemBuilder("meteorite_shard")));
     public static final RegistryObject<Item, Item> VOID_ALLOY_INGOT = ITEMS.register("void_alloy_ingot", () -> new Item(itemBuilder("void_alloy_ingot")));
     public static final RegistryObject<Item, Item> SPATIAL_WRENCH = ITEMS.register("spatial_wrench", () -> new Item(itemBuilder("spatial_wrench")));
+    public static final RegistryObject<Item, Item> RAW_AZURITE = ITEMS.register("raw_azurite", () -> new Item(itemBuilder("raw_azurite")));
+    public static final RegistryObject<Item, Item> AZURITE_INGOT = ITEMS.register("azurite_ingot", () -> new Item(itemBuilder("azurite_ingot")));
 
     public static final RegistryObject<Item, Item> GLASS_LENS = ITEMS.register("glass_lens", () -> new Item(itemBuilder("glass_lens").stacksTo(1)));
     public static final RegistryObject<Item, Item> CRYSTAL_LENS = ITEMS.register("crystal_lens", () -> new Item(itemBuilder("crystal_lens").stacksTo(1)));
