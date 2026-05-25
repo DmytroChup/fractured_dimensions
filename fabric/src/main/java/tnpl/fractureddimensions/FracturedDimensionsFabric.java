8b706f6420be94915cc6327281e7ef14a82b44bd
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2026-05-25 16:18:10 +0200

    Add worldgen for Azurite Ore and Meteorite Craters

--- fabric/src/main/java/tnpl/fractureddimensions/FracturedDimensionsFabric.java
+++ fabric/src/main/java/tnpl/fractureddimensions/FracturedDimensionsFabric.java
@@ -1,11 +1,26 @@
 package tnpl.fractureddimensions;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class FracturedDimensionsFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         CommonClass.init();
+
+        BiomeModifications.addFeature(
+                BiomeSelectors.foundInOverworld(),
+                GenerationStep.Decoration.UNDERGROUND_ORES,
+                ResourceKey.create(
+                        Registries.PLACED_FEATURE,
+                        Identifier.fromNamespaceAndPath("fractured_dimensions", "azurite_ore")
+                )
+        );
     }
 }
