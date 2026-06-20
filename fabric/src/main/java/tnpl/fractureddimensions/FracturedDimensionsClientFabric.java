6b3cf553f0a2faa8b94c2211e0578b1783510333
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2026-06-21 01:21:01 +0200

    Add Geckolib assets and client-side renderers for Photospherique

--- fabric/src/main/java/tnpl/fractureddimensions/FracturedDimensionsClientFabric.java
+++ fabric/src/main/java/tnpl/fractureddimensions/FracturedDimensionsClientFabric.java
@@ -3,15 +3,21 @@ package tnpl.fractureddimensions;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import tnpl.fractureddimensions.client.gui.AnchorControllerScreen;
 import tnpl.fractureddimensions.client.render.block.AnchorControllerRenderer;
 import tnpl.fractureddimensions.registry.ModBlockEntities;
 import tnpl.fractureddimensions.registry.ModMenus;
+import com.geckolib.renderer.GeoEntityRenderer;
+import tnpl.fractureddimensions.registry.ModEntityTypes;
 
 public class FracturedDimensionsClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MenuScreens.register(ModMenus.ANCHOR_CONTROLLER_MENU.get(), AnchorControllerScreen::new);
         BlockEntityRenderers.register(ModBlockEntities.ANCHOR_CONTROLLER.get(), AnchorControllerRenderer::new);
+
+        EntityRenderers.register(ModEntityTypes.PHOTOSPHERIQUE.get(), context -> new GeoEntityRenderer<>(context, ModEntityTypes.PHOTOSPHERIQUE.get()));
+        EntityRenderers.register(ModEntityTypes.PHOTOSPHERIQUE_RING.get(), context -> new GeoEntityRenderer<>(context, ModEntityTypes.PHOTOSPHERIQUE_RING.get()));
     }
 }
--- neoforge/src/main/java/tnpl/fractureddimensions/events/client/NeoForgeClientModEvents.java
+++ neoforge/src/main/java/tnpl/fractureddimensions/events/client/NeoForgeClientModEvents.java
@@ -6,10 +6,14 @@ import tnpl.fractureddimensions.client.gui.AnchorControllerScreen;
 import tnpl.fractureddimensions.client.render.block.AnchorControllerRenderer;
 import tnpl.fractureddimensions.registry.ModBlockEntities;
 import tnpl.fractureddimensions.registry.ModMenus;
+import com.geckolib.renderer.GeoEntityRenderer;
+import tnpl.fractureddimensions.registry.ModEntityTypes;
 
 public class NeoForgeClientModEvents {
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(ModBlockEntities.ANCHOR_CONTROLLER.get(), AnchorControllerRenderer::new);
+        event.registerEntityRenderer(ModEntityTypes.PHOTOSPHERIQUE.get(), context -> new GeoEntityRenderer<>(context, ModEntityTypes.PHOTOSPHERIQUE.get()));
+        event.registerEntityRenderer(ModEntityTypes.PHOTOSPHERIQUE_RING.get(), context -> new GeoEntityRenderer<>(context, ModEntityTypes.PHOTOSPHERIQUE_RING.get()));
     }
 
     public static void registerScreens(RegisterMenuScreensEvent event) {
