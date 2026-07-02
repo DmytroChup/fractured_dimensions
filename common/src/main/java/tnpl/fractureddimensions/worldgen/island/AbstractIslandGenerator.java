d4aeb9250cb1497a54a4a7614461347dc57d4b0a
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2026-07-02 17:15:35 +0200

    Extract Y-coordinate calculations in AbstractIslandGenerator

--- common/src/main/java/tnpl/fractureddimensions/worldgen/island/AbstractIslandGenerator.java
+++ common/src/main/java/tnpl/fractureddimensions/worldgen/island/AbstractIslandGenerator.java
@@ -30,14 +30,8 @@ public abstract class AbstractIslandGenerator implements IslandTypeGenerator {
 
                 if (!shouldGenerateAt(dist2D, maxDist)) continue;
 
-                double topNoise = noise.getValue(globalX * 0.015, 100, globalZ * 0.015);
-                int topY = center.getY() + (int) (topNoise * 4);
-
-                double depthFactor = 1.0 - (dist2D / maxDist);
-                if (depthFactor < 0) depthFactor = 0;
-                
-                double bottomNoise = noise.getValue(globalX * 0.03, -100, globalZ * 0.03);
-                int bottomY = center.getY() + (int) (-heightLimit * depthFactor * (0.6 + (bottomNoise + 1.0) / 2.0 * 0.4));
+                int topY = calculateTopY(globalX, globalZ, center, dist2D, maxDist, noise);
+                int bottomY = calculateBottomY(globalX, globalZ, center, dist2D, maxDist, heightLimit, noise);
 
                 generateColumn(chunk, lx, lz, globalX, globalZ, center, topY, bottomY, dist2D, maxDist, data, seed, noise, random);
 
@@ -52,6 +46,19 @@ public abstract class AbstractIslandGenerator implements IslandTypeGenerator {
 
     protected boolean shouldGenerateAt(double dist2D, double maxDist) {
         return dist2D <= maxDist;
+    }
+
+    protected int calculateTopY(int globalX, int globalZ, BlockPos center, double dist2D, double maxDist, SimplexNoise noise) {
+        double topNoise = noise.getValue(globalX * 0.015, 100, globalZ * 0.015);
+        return center.getY() + (int) (topNoise * 4);
+    }
+
+    protected int calculateBottomY(int globalX, int globalZ, BlockPos center, double dist2D, double maxDist, int heightLimit, SimplexNoise noise) {
+        double depthFactor = 1.0 - (dist2D / maxDist);
+        if (depthFactor < 0) depthFactor = 0;
+        
+        double bottomNoise = noise.getValue(globalX * 0.03, -100, globalZ * 0.03);
+        return center.getY() + (int) (-heightLimit * depthFactor * (0.6 + (bottomNoise + 1.0) / 2.0 * 0.4));
     }
 
     protected abstract void generateColumn(ChunkAccess chunk, int lx, int lz, int globalX, int globalZ, BlockPos center, int topY, int bottomY, double dist2D, double maxDist, DimensionData data, long seed, SimplexNoise noise, RandomSource random);
